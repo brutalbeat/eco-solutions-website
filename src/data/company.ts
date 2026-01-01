704c570afd3ae12e9b192c6ac4ba0e38cce1310c
Emil Robert Melconian <emelconi@uwo.ca>
2026-01-01 18:18:22 -0500

    update site

--- src/data/company.ts
+++ src/data/company.ts
@@ -1,10 +1,10 @@
 export const company = {
-  name: 'EcoLog Solutions',
+  name: 'Ecolog Solutions',
   tagline: 'Building Services and Solutions',
   serviceArea: 'Serving the GTA since 2018',
   phone: '+1 (647) 700-5611',
   email: 'ecologsolutions@gmail.com',
-  addressLines: [ '1-885 Lakeshore Road E, Mississauga, ON L5E 1E2, Canada'],
+  addressLines: ['1-885 Lakeshore Road E, Mississauga, ON L5E 1E2, Canada'],
 };
 
 export const companyLinks = {
