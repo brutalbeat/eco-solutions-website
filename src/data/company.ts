0f731fcca2cd7f0f7e32e48972fd09b29bb8d694
Emil Melconian <118843819+brutalbeat@users.noreply.github.com>
2026-04-03 17:29:54 -0400

    Update company.ts

--- src/data/company.ts
+++ src/data/company.ts
@@ -3,7 +3,7 @@ export const company = {
   tagline: 'Cleaning Solutions',
   serviceArea: 'Serving the GTA since 2018',
   phone: '+1 (647) 700-5611',
-  //email: 'axiomcleaningsolutions@gmail.com',
+  email: 'info@axiomcleaning.ca',
   addressLines: ['1-885 Lakeshore Road E, Mississauga, ON L5E 1E2, Canada'],
 };
 
